640fc190f6e32b4bddbf90eb43610ff9c541af12
Donneger Etienne <etienne.donneger@insa-cvl.fr>
2021-09-08 15:17:38 -0400

    Updated design. TODO : Add code for functionalities, link elements together.

--- app/src/main/java/com/example/lab1_calc/MainActivity.java
+++ app/src/main/java/com/example/lab1_calc/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.lab1_calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM, WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
         setContentView(R.layout.activity_main);
     }
 }
